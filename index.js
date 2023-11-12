3349c6b3fa1f6fe09e95b0b6ab0535f825e0a5b0
arnonr <arnonr@kmutnb.ac.th>
2023-11-12 14:11:24 +0700

    index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload());
-app.use("/static", express.static(__dirname + "/public"));
+app.use(process.env.URL_STATIC, express.static(__dirname + "/public"));
 
 app.use(routes);
 
